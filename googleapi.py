b28cc52a0d8588473cd3f379e3cbfe0d72e74f43
Shaitan5067 <51053040+Shaitan5067@users.noreply.github.com>
2019-08-21 16:25:21 +0530

    Update googleapi.py

--- googleapi.py
+++ googleapi.py
@@ -3,15 +3,12 @@
 #### You can get a csv file in the folder of restaurants ####
 
 
-import pandas as pd
 import gmaps
-import gmplot
-a=pd.read_csv("C:\\Users\\sanket sanglikar\\Downloads\\Restaurants1.csv")
+import pandas as pd
+b=pd.read_csv("C:\\Users\\sanket sanglikar\\Downloads\\Restaurants1.csv")
 gmaps.configure(api_key='AIzaSyDUu7koYtpHQ_8P8R66zfCNqLEFAOv6QBM')
-marker_locations=[]
-for i in range(len(a['latitude'])):
-    marker_locations.append([(a['longitude'][i]),(a['latitude'][i])])
-fig = gmaps.figure(map_type='ROADMAP')
-markers = gmaps.marker_layer(marker_locations)
-fig.add_layer(markers)
-fig
+marker=(b[["latitude","longitude"]])
+fig1 = gmaps.figure()
+markers1 = gmaps.marker_layer(marker)
+fig1.add_layer(markers1)
+fig1
